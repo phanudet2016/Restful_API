b5e9390fe31d0e1de205b71ddb39b9c5e1a243d7
phanudet2016 <panudach_beer_2012@hotmail.co.th>
2018-11-12 11:03:45 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,35 @@
+var express = require("express")
+var app = express()
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+var data = []
+
+app.get('/get', function (req, res) {
+  console.log('get data')
+  res.send(data)
+})
+
+app.post('/add', function (req, res) {
+    console.log('add data')
+    req.body.id = data.length + 1
+    data.push(req.body)
+    res.send(data)
+})
+
+app.put('/edit', function (req, res) {
+  if(req.body.id)
+    data[req.body.id-1] = req.body
+  console.log("edit data!")
+  res.send(data)
+})
+
+app.delete('/delete', function (req, res) {
+  if(req.body.id)
+    data.splice(req.body.id-1,1)
+  console.log("delete data!")
+  res.send(data)
+})
+
+app.listen(3000)
+console.log('My Service is listening to port 3000.')
